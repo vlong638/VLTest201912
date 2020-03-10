9e182d2426d0fe814fd769393a9da89576aa7fcd
vlong638 <vlong638@163.com>
2020-03-10 10:05:18 +0800

    ConfigurationBuilder

---
+++ VL.Console/Configuration/ConfigurationHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace VL.Consoling.Configuration
+{
+    public class ConfigurationHelper
+    {
+        public static IConfigurationRoot Build(string filename)
+        {
+            return new ConfigurationBuilder().AddJsonFile(filename).Build();
+        }
+    }
+}
---
+++ VL.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VL.Consoling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = Configuration.ConfigurationHelper.Build(@"appsettings.json");
+            Console.WriteLine(config["MessageQueue:Name"]);
+            var messageQueue = config.GetSection("MessageQueue");
+            Console.WriteLine(messageQueue["Name"]);
+
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
