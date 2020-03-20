eb19d71c2a3b4f48004f4ed131c2acc79b21e63c
vlong638 <vlong638@163.com>
2020-03-20 17:23:22 +0800

    sample of api

--- VL.API/Controllers/WeatherForecastController.cs
+++ VL.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace VL.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]/[action]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -24,7 +24,19 @@ namespace VL.API.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public WeatherForecast GetOne()
+        {
+            var rng = new Random();
+            return new WeatherForecast
+            {
+                Date = DateTime.Now,
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            };
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetSome()
         {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -34,6 +46,36 @@ namespace VL.API.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet]
+        public int GetInt()
+        {
+            return 1;
+        }
+
+        [HttpGet]
+        public List<int> GetInts()
+        {
+            return new List<int>() { 1, 2, 3 };
+        }
+
+        [HttpGet]
+        public string GetString()
+        {
+            return "s123";
+        }
+
+        [HttpGet]
+        public List<string> GetStrings()
+        {
+            return new List<string>() { "s1", "s2", "s3" };
+        }
+
+        [HttpGet]
+        public Dictionary<string, int> GetDictionary()
+        {
+            return new Dictionary<string, int>() { { "a", 1 }, { "b", 2 } };
         }
     }
 }
