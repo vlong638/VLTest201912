22322d604ce6165b70675c6d2ef68694962272b7
vlong638 <vlong638@163.com>
2020-05-14 10:05:47 +0800

    z

--- VLTest2015/Common/Models/Entities/T_PregnantInfo.cs
+++ VLTest2015/Common/Models/Entities/T_PregnantInfo.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Dapper.Contrib.Extensions;
+using System;
 
 namespace VLTest2015.Services
 {
-    public class PagedListOfPregnantInfoModel
+    [Table("T_PregnantInfo")]
+    public class T_PregnantInfo
     {
         public long Id { set; get; }
         public string PatientCode { set; get; }//病人编码
@@ -13,13 +15,13 @@ namespace VLTest2015.Services
         public int? Sex { set; get; } //性别
         public string IdCard { set; get; } //身份证号
         public string ContactPhone { set; get; } //联系人电话
-        public string Birthday { set; get; } //出生日期
+        public DateTime? Birthday { set; get; } //出生日期
         public int? Gravidity { set; get; } //孕次
         public int? Parity { set; get; } //产次
         public int? IsCreateBook { set; get; } //是否建册
         public int? GestationalWeeks { set; get; } //建册孕周
-        public string LastMenstrualPeriod { set; get; } //末次月经
-        public string DateOfPrenatal { set; get; } //预产期
+        public DateTime? LastMenstrualPeriod { set; get; } //末次月经
+        public DateTime? DateOfPrenatal { set; get; } //预产期
         public int? FileStatus { set; get; } //档案状态:(结案标识)
     }
 }
