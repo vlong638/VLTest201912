ef4c7a43a371531a01b3a3379160b77110cc682c
vlong638 <vlong@LAPTOP-NQBU1OIS>
2020-05-18 16:17:34 +0800

    配置化的下拉项方案

--- VLTest2015/Controllers/HomeController.cs
+++ VLTest2015/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
 
 namespace VLTest2015.Controllers
 {
@@ -21,6 +24,53 @@ namespace VLTest2015.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        static Dictionary<string, string> JsonConfigs = new Dictionary<string, string>();
+        /// <summary>
+        /// 获取下拉项        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="isForceChange"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public JsonResult GetDropDowns(string type,bool isForceChange)
+        {
+            //List<DropDownItem> names = new List<DropDownItem>()
+            //{
+            //    new DropDownItem("张三","张三"),
+            //    new DropDownItem("李四","李四"),
+            //    new DropDownItem("王五","王五"),
+            //};
+            //var file = (Path.Combine(AppContext.BaseDirectory, "JsonConfigs", "PersonNames.json"));
+            //System.IO.File.WriteAllText(file, Newtonsoft.Json.JsonConvert.SerializeObject(names));
+
+
+            var file = (Path.Combine(AppContext.BaseDirectory, "JsonConfigs", type + ".json"));
+            if (!System.IO.File.Exists(file))
+            {
+                List<DropDownItem> values = new List<DropDownItem>()
+                {
+                    new DropDownItem("请联系管理员配置","请联系管理员配置"),
+                };
+                System.IO.File.WriteAllText(file, Newtonsoft.Json.JsonConvert.SerializeObject(values));
+                return Json(values, JsonRequestBehavior.AllowGet);
+            }
+            var data = System.IO.File.ReadAllText(file);
+            var entity = Newtonsoft.Json.JsonConvert.DeserializeObject<List<DropDownItem>>(data);
+            return Json(entity, JsonRequestBehavior.AllowGet);
+        }
+
+        public class DropDownItem
+        {
+            public DropDownItem(string text, string value)
+            {
+                this.text = text;
+                this.value = value;
+            }
+
+            public string text { set; get; }
+            public string value { set; get; }
         }
     }
 }
