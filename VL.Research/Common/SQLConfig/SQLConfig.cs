6ed0e2360051f7493a7378dcdfce34f4eabc2f00
vlong638 <vlong@LAPTOP-NQBU1OIS>
2020-08-19 10:36:55 +0800

    1.细节修正 2.测试扩展UserList

--- VL.Research/Common/SQLConfig/SQLConfig.cs
+++ VL.Research/Common/SQLConfig/SQLConfig.cs
@@ -100,7 +100,7 @@ namespace VL.Research.Common
             foreach (var where in wheres)
             {
                 var whereConfig = Wheres.FirstOrDefault(c => c.ComponentName.ToLower() == where.Key.ToLower());
-                if (!where.Value.IsNullOrEmpty())
+                if (where != null && !where.Value.IsNullOrEmpty())
                 {
                     whereConfig.IsOn = true;
                     whereConfig.Value = where.Value;
