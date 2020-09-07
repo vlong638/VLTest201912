a880886a9013a38e46e8d1631cef9995df31fdbf
vlong638 <vlong@LAPTOP-NQBU1OIS>
2020-09-07 16:07:02 +0800

    gRPC实践案例

---
+++ gRPC.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Grpc.Core;
+using System;
+using VL.Consolo_Core.Common.ValuesSolution;
+using VLservice;
+
+namespace gRPC.Client
+{
+    class Program
+    {
+        const string Server = "localhost";
+        const int Port = 30051;
+
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel($"{Server}:{Port}", ChannelCredentials.Insecure);
+
+            var client = new VLservice.VLservice01.VLservice01Client(channel);
+            String user = "you";
+
+            var reply = client.SayHello(new HelloRequest { Name = user });
+            Console.WriteLine("FromServer, " + reply.ToJson());
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ gRPC.Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+using VL.Consolo_Core.Common.ValuesSolution;
+using VLservice;
+
+namespace gRPC.VLServer
+{
+    class VLservice01Impl : VLservice01.VLservice01Base
+    {
+        // Server side handler of the SayHello RPC
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            Console.WriteLine("From Client," + request.ToJson());
+            return Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
+        }
+    }
+
+    class Program
+    {
+        const string Server = "localhost";
+        const int Port = 30051;
+
+        static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { VLservice01.BindService(new VLservice01Impl()) },
+                Ports = { new ServerPort(Server, Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+            Console.WriteLine("Greeter server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
