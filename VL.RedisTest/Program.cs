96a9211aeb733db75e345c7f47fb7ac41c3ed724
vlong638 <vlong@LAPTOP-NQBU1OIS>
2020-09-09 15:45:03 +0800

    1.增加Redis处理类RedisCache

---
+++ VL.RedisTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using VL.Consolo_Core.Common.RedisSolution;
+
+namespace VL.RedisTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RedisCache("127.0.0.1:6379");
+            client.Set<string>("vltest", "vlvlvl", DateTime.Now.AddMinutes(5));
+            var test = client.Get<string>("vltest");
+
+
+            //var client = new RedisCache("127.0.0.1:6379,password=123456");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
