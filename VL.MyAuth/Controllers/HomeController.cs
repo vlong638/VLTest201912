60bbce0d5201b7f3c2948ff3e61d693819ba54ff
vlong638 <vlong@LAPTOP-NQBU1OIS>
2020-09-11 08:48:27 +0800

    grpc webapi样例

--- VL.MyAuth/Controllers/HomeController.cs
+++ VL.MyAuth/Controllers/HomeController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Grpc.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using VL.Consolo_Core.Common.ValuesSolution;
 using VL.MyAuth.Models;
+using VLService;
 
 namespace VL.MyAuth.Controllers
 {
@@ -20,7 +23,24 @@ namespace VL.MyAuth.Controllers
 
         public IActionResult Index()
         {
+            Channel channel = new Channel($"{Server}:{Port}", ChannelCredentials.Insecure);
+            SimpleRPCTest(channel);
+
             return View();
+        }
+
+        const string Server = "localhost";
+        const int Port = 30051;
+        void SimpleRPCTest(Channel channel)
+        {
+            var client = new VLService.VLService01.VLService01Client(channel);
+            String user = "you";
+
+            var reply = client.SayHello(new HelloRequest { Name = user });
+            Console.WriteLine("FromServer, " + reply.ToJson());
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
         }
 
         public IActionResult Privacy()
