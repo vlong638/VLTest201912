b5d4a4027d9fe1fff99295625b93feb2009847e3
vl <vlong638@163.com>
2020-10-26 11:04:03 +0800

    impl: 机构过滤样例

--- VL.Research/SQLs/湖州项目/机构过滤.sql
+++ VL.Research/SQLs/湖州项目/机构过滤.sql
@@ -1,10 +1,104 @@
+ALTER TABLE [dbo].[GY_FUWUJG] ADD [IsOnStatistics] tinyint  NULL
+GO
 
---ALTER TABLE [dbo].[GY_FUWUJG] ADD [BusinessType] tinyint NULL
---GO
+CREATE NONCLUSTERED INDEX [idx_GY_FUWUJG_BusinessType]
+ON [dbo].[GY_FUWUJG] (
+  [IsOnStatistics] ASC
+)
+GO
 
---CREATE NONCLUSTERED INDEX [idx_GY_FUWUJG_BusinessType]
---ON [dbo].[GY_FUWUJG] (
---  [BusinessType] ASC
---)
+EXEC sp_addextendedproperty
+'MS_Description', N'湖州统计过滤',
+'SCHEMA', N'dbo',
+'TABLE', N'GY_FUWUJG',
+'COLUMN', N'IsOnStatistics'
 
+
+
+
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502004'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502005'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502003'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502022'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502010'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502018'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502021'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502013'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502014'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502019'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502016'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502017'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330502009'
+
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330521006'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330521008'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330521009'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330521010'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330521012'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330521011'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330521015'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330521013'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330521016'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '47122068333052112C2201'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330521007'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330521019'
+
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330503008'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330503014'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330503004'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330503010'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330503005'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330503002'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330503006'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330503003'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330503013'
+
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523006'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523010'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523019'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523018'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523016'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523020'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523032'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523027'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523029'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523012'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523033'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523015'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523014'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523022'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523026'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330523021'
+
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522003'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522035'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522036'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522031'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522001'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522037'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522011'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522004'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522005'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522012'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522006'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522007'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522008'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522009'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522010'
+update GY_FUWUJG set HZStatisticsType = 1 where FuWuJGBH = '330522002'
+
+
+------------
+update GY_FUWUJG set HZStatisticsType = 2 where FuWuJGBH = '330502001'
+update GY_FUWUJG set HZStatisticsType = 2 where FuWuJGBH = '330502002'
+update GY_FUWUJG set HZStatisticsType = 2 where FuWuJGBH = '330502011'
+update GY_FUWUJG set HZStatisticsType = 2 where FuWuJGBH = '330502013'
+update GY_FUWUJG set HZStatisticsType = 2 where FuWuJGBH = '330502015'
+update GY_FUWUJG set HZStatisticsType = 2 where FuWuJGBH = '330502023'
+
+update GY_FUWUJG set HZStatisticsType = 2 where FuWuJGBH = '330522008'
+update GY_FUWUJG set HZStatisticsType = 2 where FuWuJGBH = 'MA29J0PK533052215A5182'
+update GY_FUWUJG set HZStatisticsType = 2 where FuWuJGBH = '330522030'
+update GY_FUWUJG set HZStatisticsType = 2 where FuWuJGBH = '330522029'
+update GY_FUWUJG set HZStatisticsType = 2 where FuWuJGBH = '330522028'
 
