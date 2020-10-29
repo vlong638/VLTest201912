18029c9b73ab64fc243f0c49ca1fdbad13e2efc7
vl <vlong638@163.com>
2020-10-29 16:44:44 +0800

    impl: Log4Net 样例

---
+++ Sample_Log4Net/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sample_Log4Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log4NetLogger.Error("1");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
