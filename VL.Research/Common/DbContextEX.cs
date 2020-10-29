924f32d70b2cea673da7e41c228e165fd66ece61
vl <vlong638@163.com>
2020-10-29 09:03:15 +0800

    fix: 追加链接异常记录

--- VL.Research/Common/DbContextEX.cs
+++ VL.Research/Common/DbContextEX.cs
@@ -14,25 +14,35 @@ namespace VL.Research.Common
         /// </summary>
         public static ServiceResult<T> DelegateTransaction<T>(this DbContext context, Func<DbGroup, T> exec)
         {
-            context.DbGroup.Connection.Open();
-            context.DbGroup.Transaction = context.DbGroup.Connection.BeginTransaction();
-            context.DbGroup.Command.Transaction = context.DbGroup.Transaction;
             try
             {
-                var result = exec(context.DbGroup);
-                context.DbGroup.Transaction.Commit();
-                context.DbGroup.Connection.Close();
-                return new ServiceResult<T>(result);
+
+                context.DbGroup.Connection.Open();
+                context.DbGroup.Transaction = context.DbGroup.Connection.BeginTransaction();
+                context.DbGroup.Command.Transaction = context.DbGroup.Transaction;
+                try
+                {
+                    var result = exec(context.DbGroup);
+                    context.DbGroup.Transaction.Commit();
+                    context.DbGroup.Connection.Close();
+                    return new ServiceResult<T>(result);
+                }
+                catch (Exception ex)
+                {
+                    context.DbGroup.Transaction.Rollback();
+                    context.DbGroup.Connection.Close();
+
+                    //集成Log4Net
+                    Log4NetLogger.Error("DelegateTransaction Exception", ex);
+
+                    return new ServiceResult<T>(default(T), ex.Message);
+                }
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                context.DbGroup.Transaction.Rollback();
-                context.DbGroup.Connection.Close();
-
                 //集成Log4Net
-                Log4NetLogger.Error("DelegateTransaction Exception", ex);
-
-                return new ServiceResult<T>(default(T), ex.Message);
+                Log4NetLogger.Error("打开数据库连接配置失败,当前数据库连接," + context.DbGroup.Connection.ConnectionString);
+                return new ServiceResult<T>(default(T), e.Message);
             }
         }
         /// <summary>
@@ -40,21 +50,29 @@ namespace VL.Research.Common
         /// </summary>
         public static ServiceResult<T> DelegateNonTransaction<T>(this DbContext context, Func<DbGroup, T> exec)
         {
-            context.DbGroup.Connection.Open();
             try
             {
-                var result = exec(context.DbGroup);
-                context.DbGroup.Connection.Close();
-                return new ServiceResult<T>(result);
+                context.DbGroup.Connection.Open();
+                try
+                {
+                    var result = exec(context.DbGroup);
+                    context.DbGroup.Connection.Close();
+                    return new ServiceResult<T>(result);
+                }
+                catch (Exception ex)
+                {
+                    context.DbGroup.Connection.Close();
+
+                    Log4NetLogger.Error("DelegateTransaction Exception", ex);
+
+                    return new ServiceResult<T>(default(T), ex.Message);
+                }
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                context.DbGroup.Connection.Close();
-
                 //集成Log4Net
-                Log4NetLogger.Error("DelegateTransaction Exception", ex);
-
-                return new ServiceResult<T>(default(T), ex.Message);
+                Log4NetLogger.Error("打开数据库连接配置失败,当前数据库连接," + context.DbGroup.Connection.ConnectionString);
+                return new ServiceResult<T>(default(T), e.Message);
             }
         }
     }
