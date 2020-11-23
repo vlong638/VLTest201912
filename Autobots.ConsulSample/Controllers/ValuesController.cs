da435d6769f076df7ede7506d8e043df05f522ab
vl <vlong638@163.com>
2020-11-23 14:51:57 +0800

    doc：framework调用consul演示

--- Autobots.ConsulSample/Controllers/ValuesController.cs
+++ Autobots.ConsulSample/Controllers/ValuesController.cs
@@ -18,6 +18,32 @@ namespace Autobots.ConsulSample.Controllers
         // GET api/values/5
         public string Get(int id)
         {
+
+            //获取所有注册的服务
+            using (var consul = new Consul.ConsulClient(c =>
+            {
+                //c.Address = new Uri($"http://{consulAddress}:{consulPort}");
+            }))
+            {
+                //取在Consul注册的全部服务
+                var services = consul.Agent.Services().Result.Response;
+                //foreach (var s in services.Values)
+                //{
+                //    var serviceNodes = services.Values.Where(c => c.Service.ToLower() == serviceName.ToLower()).ToList();
+                //    if (serviceNodes.Count() > 0)
+                //    {
+                //        var serviceNode = serviceNodes[DateTime.Now.Millisecond % serviceNodes.Count()];
+                //        return new ServiceConfig()
+                //        {
+                //            Name = serviceNode.Service,
+                //            Address = serviceNode.Address,
+                //            Port = serviceNode.Port,
+                //        };
+                //    }
+                //    Console.WriteLine($"ID={s.ID},Service={s.Service},Addr={s.Address},Port={s.Port}");
+                //}
+            }
+
             return "value";
         }
 
