6f72c28c0ae02c5656ddf0b32468972a1b23ebe7
vl <vlong638@163.com>
2020-12-07 18:50:20 +0800

    impl: 1207杭妇院体重导出

---
+++ Tester/Data.cs
@@ -0,0 +1,22 @@
+﻿using FrameworkTest.Common.ValuesSolution;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Tester
+{
+    public class Data
+    {
+        public static void AddData(List<Grouper> grouper)
+        {
+            var data = File.ReadAllLines(@"C:\Users\Administrator\Desktop\杭妇院\1207.csv");
+            foreach (string line in data)
+            {
+                var values = line.Split(',');
+                grouper.Add(new Grouper(values[0], values[3].ToInt() ?? 0, values[4] == "NULL" ? "" : values[4], values[5]));
+
+
+            }
+
+        }
+    }
+}
