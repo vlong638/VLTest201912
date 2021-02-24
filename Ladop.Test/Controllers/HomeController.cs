bae6948b1a0f30e9dc2709d313cdf5330966af18
vl <vlong638@163.com>
2021-02-24 10:19:47 +0800

    基于文件代码的打印方案

--- Ladop.Test/Controllers/HomeController.cs
+++ Ladop.Test/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Ladop.Test.Models;
+using System.IO;
+using System.Data;
+using System.Text;
 
 namespace Ladop.Test.Controllers
 {
@@ -32,6 +35,31 @@ namespace Ladop.Test.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public JsonResult PrintFile(string type)
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Docs\\打印代码生成.txt");
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var fileContent = Encoding.GetEncoding("GB2312").GetString(System.IO.File.ReadAllBytes(filePath)) ;
+            var data = GetData();
+            var line = data.Rows[0];
+            fileContent = fileContent.Replace("#姓名", data.Rows[0]["姓名"].ToString());
+            return Json(fileContent);
+        }
+
+        private DataTable GetData()
+        {
+            var dt = new DataTable();
+            var name = "姓名";
+            dt.Columns.Add(name);
+            dt.Rows.Add(dt.NewRow());
+            dt.Rows[0][name] = "张三";
+            dt.Rows.Add(dt.NewRow());
+            dt.Rows[1][name] = "李四";
+            dt.Rows.Add(dt.NewRow());
+            dt.Rows[2][name] = "王五";
+            return dt;
         }
     }
 }
